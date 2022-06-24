7547b6ba2d786a3d02a1896ad61ae7aba1f21db8
Nestertsov Maksym <nestertsovmaxim@gmail.com>
2022-06-24 16:58:31 +0300

    add timer and change mechanizm of game

--- Clicker/Clicker/Game.swift
+++ Clicker/Clicker/Game.swift
@@ -14,21 +14,28 @@ class Game: UIViewController {
     @IBOutlet private var redButton: UIButton!
     @IBOutlet private var blueButton: UIButton!
     
+    @IBOutlet var timerLabel: UILabel!
     private var counter: Int = 0
+    var timer: Timer?
+    var isTimerStarted: Bool = false
+    var timerCounter = 30
     
     override func viewDidLoad() {
         super.viewDidLoad()
         updateCounter()
+        startTimer()
     }
     
     @IBAction func tapGreenButton(_ sender: UIButton) {
         playGame()
         updateCounter()
+        startTimer()
     }
     
     @IBAction func tapRedButton(_ sender: UIButton) {
         playGame()
         updateCounter()
+        startTimer()
     }
     
     @IBAction func tapBlueButton(_ sender: UIButton) {
@@ -50,14 +57,39 @@ class Game: UIViewController {
         scoreValue.text = "\(counter)"
     }
     
-
-    
-
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        guard segue.identifier == "sgGameOver" else { return }
+        guard segue.identifier == "goToGameOver" else { return }
         guard let destVC = segue.destination as? GameOver else { return }
         destVC.valueScoreGame = scoreValue.text
-
+    }
+    
+    
+    @objc func timerTickHandler() {
+        if isTimerStarted == true, timerCounter > 1 {
+            timerCounter -= 1
+            updateCounterTimerLabel()
+        } else {
+           stopTimer()
+         self.performSegue(withIdentifier: "goToGameOver", sender: self)
+        }
+    }
+    
+    func startTimer() {
+        guard !isTimerStarted else { return }
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerTickHandler), userInfo: nil, repeats: true)
+        updateCounterTimerLabel()
+        isTimerStarted = true
+    }
+    
+    func updateCounterTimerLabel() {
+        timerLabel.text = "\(timerCounter)"
+    }
+    
+    func stopTimer() {
+        timer?.invalidate()
+        timer = nil
+        timerCounter = 0
+        isTimerStarted = false
     }
 
 }
